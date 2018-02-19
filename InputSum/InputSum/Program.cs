0fa6c95b0b9cc6a8e005a5027d01d0a864b8a0f4
Jesse Rushin <jdr323@gmail.com>
2018-02-19 17:08:53 -0500

    Add InputSum exercise to repository
    More fun with loops!

---
+++ InputSum/InputSum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace InputSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Write a program and continuously ask the user to enter a number or "ok" to exit. 
+             * Calculate the sum of all the previously entered numbers and display it on the console.*/
+        
+            var sum = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number or \"OK\" to exit.");
+                var input = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(input) || input.ToLower() == "ok")
+                    break;
+
+                sum += Convert.ToInt32(input);
+            }
+            Console.WriteLine("The sum of all previously entered numbers is {0}.", sum);
+        }
+    }
+}
