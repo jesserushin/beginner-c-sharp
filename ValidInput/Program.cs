37de4dc227a35bf256f86df7534a69125f0d2dda
jesserushin <jdr323@gmail.com>
2018-02-18 22:22:35 -0500

    Add files via upload

---
+++ ValidInput/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ValidInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number between 1 and 10.");
+            var input = Convert.ToInt32(Console.ReadLine());
+
+            if(input >= 1 && input <= 10)
+                Console.WriteLine("Valid input.");
+            else
+                Console.WriteLine("Invalid input.");
+            Console.WriteLine("Press any key.");
+            Console.Read();
+        }
+    }
+}
